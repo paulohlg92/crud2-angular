306cf13621c0e9e7883efdbf37804325dfa24699
Paulo Henrique <phguilherme92@gmail.com>
2021-05-06 11:47:36 -0300

    First Commit

---
+++ src/app/components/tutorial-list/tutorial-list.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { Tutorial } from '../../models/tutorial.model'
+import { TutorialService } from '../../services/tutorial.service'
+
+@Component({
+  selector: 'app-tutorial-list',
+  templateUrl: './tutorial-list.component.html',
+  styleUrls: ['./tutorial-list.component.css']
+})
+export class TutorialListComponent implements OnInit {
+
+  tutorials?: Tutorial[];
+  currentTutorial?: Tutorial;
+  currentIndex = -1;
+  title = '';
+
+
+  constructor(private tutorialService: TutorialService) { }
+
+  ngOnInit(): void {
+    this.retrieveTutorials();
+  }
+
+  retrieveTutorials(): void {
+    this.tutorialService.getAll()
+      .subscribe(
+        data => {
+          this.tutorials = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+  refreshList(): void {
+    this.retrieveTutorials();
+    this.currentTutorial = undefined;
+    this.currentIndex = -1;
+  }
+  setActiveTutorial(tutorial: Tutorial, index: number): void {
+    this.currentTutorial = tutorial;
+    this.currentIndex = index;
+  }
+  removeAllTutorials(): void {
+    this.tutorialService.deleteAll()
+      .subscribe(
+        response => {
+          console.log(response);
+          this.refreshList();
+
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  searchTitle(): void {
+    this.tutorialService.findByTitle(this.title)
+      .subscribe(
+        data => {
+          this.tutorials = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+}
